a2ccfbb6e7769f6aaeec142dbf50a5902621df40
shivang0130 <shivang0130@gmail.com>
2026-03-17 00:53:52 +0530

    add primitive type examples

---
+++ 01-basics/variables.ts
@@ -0,0 +1,15 @@
+// TypeScript primitive types
+
+let username: string = "Shivang"
+let age: number = 20
+let isDeveloper: boolean = true
+
+console.log(username)
+console.log(age)
+console.log(isDeveloper)
+
+// Type inference example
+
+let city = "Meerut"
+
+console.log(city)
